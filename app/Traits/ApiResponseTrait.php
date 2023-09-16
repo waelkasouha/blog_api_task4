e1b6537b1bbccc32965f78b7cf66e4dfdf3cbb85
wael kasouha <waelkas99@gmail.com>
2023-09-16 11:56:49 -0400

    create PostController

---
+++ app/Traits/ApiResponseTrait.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Traits;
+
+trait ApiResponseTrait
+{
+    public function apiResponse($data= null, $message = null, $status= null){
+        $array = [
+            'data' =>$data,
+            'message'=>$message,
+            'Status'=>$status ,
+        ];
+        return response($array);
+    }
+}
